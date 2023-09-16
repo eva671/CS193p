869ee76b11b29fc9fe7a7d8591db110f9e1ec962
Eva Liu <qliu1996@gmail.com>
2023-09-17 05:28:10 +0900

    Finished required task 1 of Assignment 1

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -8,16 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🫡","🥹","😤","🥲"]
+    let emojis = ["🫡","🥹","😤","🥲","🤣","😶‍🌫️","😊","🤯" ]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack{
-            ForEach(emojis.indices, id: \.self) { index in 
+        VStack{
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View { //this is a normal function
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) { //this is a view builder
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange) //default is to fill the background with orange too
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{ //by is what the caller uses; offset is the user-defined name, internal vs external parameter names. Symbol is both internal and external
+        Button(action: {
+                cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
@@ -30,13 +70,13 @@ struct CardView: View {
     var body: some View {
         ZStack { //trailing closure syntax - dont need a () because last/only argument is a function.
             let base = RoundedRectangle(cornerRadius: 12) //a View cannot be changed, so use let instead of var. Type Inference.
-            if isFaceUp{
+            Group {
                 base.foregroundColor(.white) //overwrite the ground Colour on line 19
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill() // take orange colour from line 19
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1) //make the emojis transparent when face down so that the cards don't shrink - emojis are still there.
 
         }
         .onTapGesture {
